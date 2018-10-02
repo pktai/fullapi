7022c48e50d423ed5c2ba148da8b7bb2560a4552
pktai <36804773+pktai@users.noreply.github.com>
2018-10-02 09:51:16 +0700

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,5 @@
 const config = {
     port: 8000,
-    urlMongo: 'mongodb://localhost:27017/api-stream',
+    urlMongo: 'mongodb://<pktai>:<pkt19031996>@ds161008.mlab.com:61008/testapi',
 }
 export default config;
